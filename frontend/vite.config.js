b1c2cd00c56348fd715ef810eb75f93706aefce6
wispard1 <antonvlasov610@gmail.com>
2025-10-25 17:45:49 +0800

    update vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,12 +6,27 @@ export default defineConfig({
   server: {
     port: 5002,
     proxy: {
+      // Проксируем запросы к API
       '/api/v1': {
         target: 'http://localhost:5001',
         changeOrigin: true,
       },
+      // Проксируем WebSocket соединения
       '/socket.io': {
         target: 'ws://localhost:5001',
+        ws: true,
+      },
+    },
+  },
+  preview: {
+    port: 10000,
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'ws://localhost:3000',
         ws: true,
       },
     },
