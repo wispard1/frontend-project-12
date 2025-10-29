547ae2a2789a50e338f6e697e99b7512d42b1e62
wispard1 <antonvlasov610@gmail.com>
2025-10-29 16:54:33 +0800

    fix adding channel error test

--- frontend/src/components/ChannelsList.jsx
+++ frontend/src/components/ChannelsList.jsx
@@ -19,24 +19,25 @@ export const ChannelsList = ({
       <div className='d-flex justify-content-between align-items-center px-3 py-2 border-bottom'>
         <b>{t('chatPage.channelsHeader')}</b>
         <Button
-          variant='link'
-          className='p-0 text-primary'
+          variant='outline-primary'
+          className='p-0 d-flex align-items-center justify-content-center'
           onClick={onAddChannelClick}
           title={t('chatPage.addChannel')}
           data-testid='add-channel-button'
+          style={{
+            width: '24px',
+            height: '24px',
+            fontSize: '1.2rem',
+            fontWeight: 'bold',
+            border: '1px solid #0d6efd',
+            lineHeight: 1,
+            padding: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
         >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 16 16'
-            width='20'
-            height='20'
-            fill='currentColor'
-            className='bi bi-plus-square'
-          >
-            <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'></path>
-            <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4'></path>
-          </svg>
-          <span style={{ position: 'absolute', left: '-9999px' }}>+</span>
+          +
         </Button>
       </div>
       <ListGroup className='overflow-auto px-2 py-2 flex-grow-1'>
