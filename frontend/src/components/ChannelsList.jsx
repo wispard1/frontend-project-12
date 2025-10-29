aa96347681b816142ccc560e47a8ae2bc721357d
wispard1 <antonvlasov610@gmail.com>
2025-10-29 19:15:41 +0800

    fix adding channel profanity

--- frontend/src/components/ChannelsList.jsx
+++ frontend/src/components/ChannelsList.jsx
@@ -55,13 +55,10 @@ export const ChannelsList = ({
               padding: '0.5rem 0.75rem',
             }}
             onClick={() => onChannelClick(channel.id)}
-            aria-label={channel.name}
+            // aria-label={channel.name}
             data-testid={`channel-${channel.name}`}
           >
-            <span className='text-truncate'>
-              <span className='me-1'>#</span>
-              {channel.name}
-            </span>
+              <span># {channel.name}</span>
             {channel.removable && (
               <Dropdown onClick={(e) => e.stopPropagation()}>
                 <Dropdown.Toggle
@@ -78,12 +75,7 @@ export const ChannelsList = ({
                     fill='currentColor'
                     className='bi bi-chevron-down'
                     viewBox='0 0 16 16'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
-                    />
-                  </svg>
+                  ></svg>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
                   <Dropdown.Item onClick={() => onRenameChannelClick(channel.id, channel.name)}>
