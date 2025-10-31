f22d057d14e6c235491b2d27c58b879fbcdbb1ed
wispard1 <antonvlasov610@gmail.com>
2025-10-31 09:52:29 +0800

    refactor: update i18n config after moving locales to src

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,16 +1,16 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import Backend from 'i18next-http-backend'
+import ruTranslation from './locales/ru/translation.json'
 
 i18n
-  .use(Backend)
   .use(initReactI18next)
   .init({
     lng: 'ru',
     fallbackLng: 'ru',
-    supportedLngs: ['ru'],
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    resources: {
+      ru: {
+        translation: ruTranslation,
+      },
     },
     interpolation: {
       escapeValue: false,
